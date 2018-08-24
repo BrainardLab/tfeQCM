eea0d63153402a986aa280610e250ad981002c79
Michael Barnett <micalan@sas.upenn.edu>
2018-08-24 14:53:10 -0400

    updated paramsToVec and vecToParams for 2d case

--- Models/QCM/@tfeQCM/paramsToVec.m
+++ Models/QCM/@tfeQCM/paramsToVec.m
@@ -15,19 +15,35 @@ p.addParameter('UseNoiseParam',false,@islogical);
 p.parse(params,varargin{:});
 params = p.Results.params;
 
-% Take the parameter structure into a vector
-for i = 1:5
-    x(i) = params.Qvec(i);
+switch obj.dimension
+    case 3
+        % Take the parameter structure into a vector
+        for i = 1:5
+            x(i) = params.Qvec(i);
+        end
+        x(6) = params.crfAmp;
+        x(7) = params.crfExponent;
+        x(8) = params.crfSemi;
+        x(9) = params.expFalloff;
+        x(10) = params.offset;
+        % Optional inclusion of noise
+        if (p.Results.UseNoiseParam)
+            x(11) = params.noiseSd;
+        end
+    case 2
+        for i = 1:3
+            x(i) = params.Qvec(i);
+        end
+        x(4) = params.crfAmp;
+        x(5) = params.crfExponent;
+        x(6) = params.crfSemi;
+        x(7) = params.expFalloff;
+        x(8) = params.offset;
+        % Optional inclusion of noise
+        if (p.Results.UseNoiseParam)
+            x(9) = params.noiseSd;
+        end
 end
-x(6) = params.crfAmp;
-x(7) = params.crfExponent;
-x(8) = params.crfSemi;
-x(9) = params.expFalloff;
-x(10) = params.offset;
 
-% Optional inclusion of noise
-if (p.Results.UseNoiseParam)
-    x(11) = params.noiseSd;
-end
 
 end
--- Models/QCM/@tfeQCM/vecToParams.m
+++ Models/QCM/@tfeQCM/vecToParams.m
@@ -15,15 +15,29 @@ p.addParameter('UseNoiseParam',false,@islogical);
 p.parse(x,varargin{:});
 x = p.Results.x;
 
-params.Qvec(1:5) = x(1:5)';
-params.crfAmp = x(6);
-params.crfExponent = x(7);
-params.crfSemi = x(8);
-params.expFalloff = x(9);
-params.offset = x(10);
-% Optional inclusion of noise
-if (p.Results.UseNoiseParam)
-    params.noiseSd = x(11);
+switch obj.dimension
+    case 3
+        params.Qvec(1:5) = x(1:5)';
+        params.crfAmp = x(6);
+        params.crfExponent = x(7);
+        params.crfSemi = x(8);
+        params.expFalloff = x(9);
+        params.offset = x(10);
+        % Optional inclusion of noise
+        if (p.Results.UseNoiseParam)
+            params.noiseSd = x(11);
+        end
+    case 2
+        params.Qvec(1:3) = x(1:3)';
+        params.crfAmp = x(4);
+        params.crfExponent = x(5);
+        params.crfSemi = x(6);
+        params.expFalloff = x(7);
+        params.offset = x(8);
+        % Optional inclusion of noise
+        if (p.Results.UseNoiseParam)
+            params.noiseSd = x(9);
+        end
 end
-
+        
 end
