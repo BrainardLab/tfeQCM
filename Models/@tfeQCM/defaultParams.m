5d08c18f98b75d3dc8ed782297e140ec6edd26b2
David Brainard <brainard@psych.upenn.edu>
2018-08-30 16:52:42 -0400

    Allow passing of key/value pair.  Needs commenting.

--- Models/@tfeQCM/defaultParams.m
+++ Models/@tfeQCM/defaultParams.m
@@ -7,9 +7,46 @@ function [params,paramsLb,paramsUb] = defaultParams(obj,varargin)
 % All three returns are in struct form, use paramsToVec on the structs to
 % get vector form.
 
-%% Default parameters for Q
-%
-% This is dimension specific
+%% COMMENT DEFAULT PARAMS KEY/VALUE PAIR AS WELL NEW CODE.
+
+% Parse vargin for options passed here
+p = inputParser; p.KeepUnmatched = true;
+p.addParameter('defaultParams',[],@(x)(isempty(x) | isstruct(x)));
+p.parse(varargin{:});
+
+if (isempty(p.Results.defaultParams))
+    %% Default parameters for Q
+    %
+    % This is dimension specific
+    switch obj.dimension
+        case 3
+            % Quadratic parameters
+            %
+            % We only store 5 because we handle the amplitude of the response
+            % in the amplitude parameter below.  The first axis of the ellipse
+            % has an implicit value of 1.
+            params.Qvec = [1 1 0 0 0];
+            
+        case 2
+            params.Qvec = [1 0 ];
+    end
+    
+    %% The Naka-Rushton
+    params.crfAmp = 1;
+    params.crfSemi = 1;
+    params.crfExponent = 2;
+    params.offset = 0;
+    
+    % Exponential falloff (not used?)
+    params.expFalloff = 0.3;
+    
+    % Noise level (used for simulations)
+    params.noiseSd = 0.2;
+    
+else
+    params = p.Results.defaultParams;
+end
+
 switch obj.dimension
     case 3
         % Quadratic parameters
@@ -17,28 +54,14 @@ switch obj.dimension
         % We only store 5 because we handle the amplitude of the response
         % in the amplitude parameter below.  The first axis of the ellipse
         % has an implicit value of 1.
-        params.Qvec = [1 1 0 0 0];
         paramsLb.Qvec = [1e-2 1e-2 -360 -360 -360];
         paramsUb.Qvec = [1e2 1e2 360 360 360];
-
+        
         
     case 2
-        params.Qvec = [1 0 ];
         paramsLb.Qvec = [1e-2 -360];
         paramsUb.Qvec = [1e2 360];
 end
-
-%% The Naka-Rushton
-params.crfAmp = 1;
-params.crfSemi = 1;
-params.crfExponent = 2;
-params.offset = 0; 
-
-% Exponential falloff (not used?)
-params.expFalloff = 0.3;
-
-% Noise level (used for simulations)
-params.noiseSd = 0.2;
 
 %% Lower bounds
 paramsLb.crfAmp = 1e-1;
@@ -54,5 +77,5 @@ paramsUb.crfSemi = 1e2 ;
 paramsUb.crfExponent = 10;
 paramsUb.expFalloff = 1e1;
 paramsUb.noiseLevel = 100;
-paramsUb.offset = 2; 
+paramsUb.offset = 2;
 end
