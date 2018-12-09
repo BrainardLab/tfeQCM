9fc29d3164d9b66343a63047661b2f0fd17c92c8
David Brainard <brainard@psych.upenn.edu>
2018-12-09 14:43:51 -0500

    Allow full set of parameters

--- Toolbox/tfeNRParamsToVec.m
+++ Toolbox/tfeNRParamsToVec.m
@@ -7,6 +7,6 @@ function paramsvec = tfeNRParamsToVec(NRParams)
     nIndDirections = length(NRParams);
     paramsvec = [];
     for ii = 1:nIndDirections
-        paramsvec = [paramsvec ; [NRParams(ii).crfAmp NRParams(ii).crfSemi NRParams(ii).crfExponent NRParams(ii).crfOffset]'];
+        paramsvec = [paramsvec ; [NRParams(ii).crfAmp NRParams(ii).crfSemi NRParams(ii).crfExponent NRParams(ii).crfOffset NRParams(ii).expFalloff NRParams(ii).noiseSd]'];
     end
 end
