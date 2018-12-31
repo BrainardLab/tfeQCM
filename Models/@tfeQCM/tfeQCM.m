3821d3b0cb03eaff356a7c7270427d56e0cf916d
David Brainard <brainard@psych.upenn.edu>
2018-12-31 08:57:32 -0500

    Put in hooks for locking parameters
    But doing this right is going to take a little more time.

--- Models/@tfeQCM/tfeQCM.m
+++ Models/@tfeQCM/tfeQCM.m
@@ -20,6 +20,24 @@ classdef tfeQCM < tfe
 % Optional key/value pairs:
 %    'dimension' - The dimension of the ellipsoid. Can be 2 or 3. Default
 %                  3.
+%    'lockedAngle' - If dimension is 2, this specifies the angle of the
+%                    ellipse, and this angle is not searched over. Default
+%                    is empty, which means angle is searched over.
+%    'lockedCrfAmp' - If value set, this is used as the amplitude of the
+%                    Naka-Rrushton function, and that is not searched over.
+%                    Default is empty, which means search.
+%    'lockedCrfExponent' - If value set, this is used as the exponent of
+%                    the Naka-Rushton function, and that is not searched
+%                    over. Default is empty, which means search.
+
+%    'lockedCrfSemi' - If value set, this is used as the semi-saturation
+%                    constant of the Naka-Rushton function, and that is not
+%                    searched over. Default is empty, which means search.
+
+%    'lockedCrfOffset' - If value set, this is used as the offset of
+%                    the Naka-Rushton function, and that is not searched
+%                    over. Default is empty, which means search.
+%
 % See also:
 %
 
@@ -31,6 +49,15 @@ classdef tfeQCM < tfe
     properties (SetAccess = protected, GetAccess = public)
         % Specify dimension of ellipsoid.  Can be 2 or 3.
         dimension = 3;
+        
+        % Locked angle (if dimension is 2)
+        lockedAngle = [];
+        
+        % Locked Naka-Rushton parameters
+        lockedCrfAmp = [];
+        lockedCrfExponent = [];
+        lockedCrfSemi = [];
+        lockedCrfOffset = [];
     end
     
     % Private properties. Only methods of the parent class can set these
@@ -56,6 +83,11 @@ classdef tfeQCM < tfe
             p = inputParser; p.KeepUnmatched = true;
             p.addParameter('verbosity','none',@ischar);
             p.addParameter('dimension',3,@(x) (isnumeric(x) & isscalar(x)));
+            p.addParameter('lockedAngle',[],@isnumeric);
+            p.addParameter('lockedCrfAmp',[],@isnumeric);
+            p.addParameter('lockedCrfExponent',[],@isnumeric);
+            p.addParameter('lockedCrfSemi',[],@isnumeric);
+            p.addParameter('lockedCrfOffset',[],@isnumeric);
             p.parse(varargin{:});
             
             % Base class constructor
@@ -66,14 +98,22 @@ classdef tfeQCM < tfe
             if (obj.dimension ~= 2 & obj.dimension ~= 3)
                 error('Can only handle dimension 2 or 3');
             end
-
+            
+            % Set locks
+            obj.lockedAngle = p.Results.lockedAngle;
+            if (~isempty(obj.lockedAngle & obj.dimension ~= 2)
+                error('Locking angle only works for dimension 2');
+            end
+            obj.lockedCrfAmp = p.Results.lockedCrfAmp;
+            obj.lockedCrfExponent = p.Results.lockedCrfExponente;
+            obj.lockedCrfSemi = p.Results.lockedCrfSemi;
+            obj.lockedCrfOffset = p.Results.lockedCrfOffset;
         end
         
         % Override tfe fitError so we can scale appropriately for this
         % problem.  This makes a difference to fmincon's behavior, but
         % I did not want to change the tfe function itself. 
         function [fVal,modelResponseStruct] = fitError(obj,paramsVec,thePacket,varargin)
-
             [fVal,modelResponseStruct] = fitError@tfe(obj,paramsVec,thePacket,varargin{:});
             fVal= 1000*fVal;
         end
