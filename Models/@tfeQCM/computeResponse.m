805a764ba599fbad9fdfac097ad494bc7d63853f
David Brainard <brainard@psych.upenn.edu>
2019-09-05 15:37:33 -0400

    Insert fancy correlated noise option from IAMP mode in tfe

--- Models/@tfeQCM/computeResponse.m
+++ Models/@tfeQCM/computeResponse.m
@@ -6,6 +6,15 @@ function modelResponseStruct = computeResponse(obj,params,stimulusStruct,kernelS
 % Optional key/value pairs
 %   'addNoise' - true/false (default false).  Add noise to computed
 %                response?  Useful for simulations.
+%
+% The field 'noiseInverseFrequencyPower' can be set in the parameters, and
+%   will dictate the form of the stimulated noise. Noise is generated using
+%   the dsp.ColoredNoise. This parameter determines alpha, the
+%   exponent of the noise generation property, following the formula:
+%   noise = 1/|f|^alpha. The default value of zero produces white noise.
+%   A value of 1 produces 'pink' noise, and a value of 2 'brown'(ian)
+%   noise. Functional MRI data have autocorrelated noise, with an
+%   alpha value on the order of 1 or less.
 
 %% Parse input
 %
@@ -31,9 +40,14 @@ modelResponseStruct.values = neuralResponse;
 modelResponseStruct = obj.applyKernel(modelResponseStruct,kernelStruct,varargin{:});
 
 %% Optional add noise
-if (p.Results.addNoise)
-    modelResponseStruct.values = modelResponseStruct.values + normrnd(0,params.noiseSd,size(modelResponseStruct.values));
+if p.Results.addNoise
+    if ~isfield(params, 'noiseInverseFrequencyPower')
+        params.noiseInverseFrequencyPower=0;
+    end
+    cn = dsp.ColoredNoise(params.noiseInverseFrequencyPower,length(modelResponseStruct.timebase),1);
+    noise = step(cn)';
+    noise = noise/std(noise)*params.noiseSd;
+    modelResponseStruct.values = modelResponseStruct.values + noise;
 end
-
 
 end
