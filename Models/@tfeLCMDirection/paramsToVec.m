3c1c9f8f1179de133ffcfa0c5261965f924190fd
David Brainard <brainard@psych.upenn.edu>
2021-03-12 10:05:52 -0500

    Put back right version.

--- Models/@tfeLCMDirection/paramsToVec.m
+++ Models/@tfeLCMDirection/paramsToVec.m
@@ -1,17 +1,17 @@
-function paramsOut = scaleIsoContrast(obj,params,scalar,varargin)
-% paramsOut = scaleIsoContrast(obj,params,varargin)
+function x = paramsToVec(obj,params,varargin)
+% x = paramsToVec(obj,params,varargin)
 %
-% Description:
-%    Scale the parameters so as to change the size of the isoresponse
-%    contour by the passed scalar,without changing the model's predictions.
+% Convert vector form of parameters to struct
 %
 % Key/value pairs
+%   'UseNoiseParam' - true/false (default false).  Use the noise parameter?
 %
-% 03/12/21  dhb  Wrote it.
+% 03/01/21  dhb  Wrote it from QCM version.
 
 % Parse input.
 p = inputParser;
 p.addRequired('params',@isstruct);
+p.addParameter('UseNoiseParam',false,@islogical);
 p.parse(params,varargin{:});
 params = p.Results.params;
 
@@ -20,15 +20,22 @@ if (obj.dimension ~= 2)
     error('LCM only implemented in 2 dimensions');
 end
 
-% Check
-if (scalar < 1e-6)
-    error('That''s an awfully small scalar');
+% Expand-eroo!
+for i = 1:obj.nChannels/2
+    x(i) = params.channelWeightsPos(i);
+end
+x(obj.nChannels/2+1) = params.crfAmp;
+x(obj.nChannels/2+2) = params.crfExponent;
+x(obj.nChannels/2+4) = params.crfSemi;
+x(obj.nChannels/2+5) = params.expFalloff;
+x(obj.nChannels/2+5) = params.crfOffset;
+
+% Optional inclusion of noise
+if (p.Results.UseNoiseParam)
+    x(obj.nChannels/2+6) = params.noiseSd;
 end
 
-% Adjust weights and compensate with semi-satauration constant
-paramsOut = params;
-paramsOut.channelWeightsPos = params.channelWeightsPos/scalar;
-paramsOut.crfSemi = params.crfSemi/scalar;
-
+% Transpose to match convention
+x = x';
 
 end
