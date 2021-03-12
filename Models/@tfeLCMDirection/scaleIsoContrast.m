b08fa0f9e56d8714ee00fc8f5a0d9c56be99ea3d
David Brainard <brainard@psych.upenn.edu>
2021-03-12 08:21:38 -0500

    Wrote it.

---
+++ Models/@tfeLCMDirection/scaleIsoContrast.m
@@ -0,0 +1,34 @@
+function paramsOut = scaleIsoContrast(obj,params,scalar,varargin)
+% paramsOut = scaleIsoContrast(obj,params,varargin)
+%
+% Description:
+%    Scale the parameters so as to change the size of the isoresponse
+%    contour by the passed scalar,without changing the model's predictions.
+%
+% Key/value pairs
+%
+% 03/12/21  dhb  Wrote it.
+
+% Parse input.
+p = inputParser;
+p.addRequired('params',@isstruct);
+p.parse(params,varargin{:});
+params = p.Results.params;
+
+% Dimension check
+if (obj.dimension ~= 2)
+    error('LCM only implemented in 2 dimensions');
+end
+
+% Check
+if (scalar < 1e-6)
+    error('That''s an awfully small scalar');
+end
+
+% Adjust weights and compensate with semi-satauration constant
+paramsOut = params;
+paramsOut.channelWeightsPos = params.channelWeightsPos/scalar;
+paramsOut.crfSemi = params.crfSemi/scalar;
+
+
+end
